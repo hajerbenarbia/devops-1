b9c28c4e80f06fb19a112947ad249c799c24843e
imenbenabdlaziz <57324348+imenbenabdlaziz@users.noreply.github.com>
2021-11-12 09:56:11 +0100

    Junit test

---
+++ src/main/java/tn/esprit/spring/services/IMissionService.java
@@ -0,0 +1,10 @@
+package tn.esprit.spring.services;
+
+import tn.esprit.spring.entities.Mission;
+
+public interface IMissionService {
+	
+	public int ajouterMission(Mission mission);
+	public String deleteMissionById(int missionId);
+	public String getMissionNameById(int missionId);
+}
